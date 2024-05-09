432e166501bd13cff3234692c67c8df1d54b833c
parthwps <parth@netdevs.net>
2024-05-09 16:22:12 +0530

    cleanup

--- header.php
+++ header.php
@@ -13,13 +13,6 @@ require_once('function.php');
 $user = selectFromTable('users', ['*'], ['id' => $_SESSION['user_id']]);
 $user = $user[0];
 
-// Logout functionality
-if (isset($_POST['logout'])) {
-    session_destroy();
-    header("Location: index.php");
-    exit();
-}
-
 // Check user status
 $isFaculty = isset($_SESSION['user_status']) && $_SESSION['user_status'] === 'faculty';
 ?>
@@ -83,127 +76,11 @@ $isFaculty = isset($_SESSION['user_status']) && $_SESSION['user_status'] === 'fa
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      <!-- Navbar Search -->
-      <!-- <li class="nav-item">
-        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-          <i class="fas fa-search"></i>
-        </a>
-        <div class="navbar-search-block">
-          <form class="form-inline">
-            <div class="input-group input-group-sm">
-              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-              <div class="input-group-append">
-                <button class="btn btn-navbar" type="submit">
-                  <i class="fas fa-search"></i>
-                </button>
-                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </li> -->
-
-      <!-- Messages Dropdown Menu -->
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item"> -->
-            <!-- Message Start -->
-            <!-- <div class="media">
-              <img src="./asset/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div> -->
-            <!-- Message End -->
-          <!-- </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item"> -->
-            <!-- Message Start -->
-            <!-- <div class="media">
-              <img src="./asset/img/user4-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div> -->
-            <!-- Message End -->
-          <!-- </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item"> -->
-            <!-- Message Start -->
-            <!-- <div class="media">
-              <img src="./asset/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div> -->
-            <!-- Message End -->
-          <!-- </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li> -->
-      <!-- Notifications Dropdown Menu -->
-      <!-- <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li> -->
-            <ul class="navbar-nav ml-auto">
-        <li class="nav-item d-flex align-items-center">
-          <span class="mr-2"><?php echo $user['fullname']; ?></span>
-          <form method="POST" style="margin-bottom: 0;">
-            <button class="nav-link" type="submit" name="logout" style="
-            text-decoration: none;
-            border: none;
-            background-color: transparent;
-            padding: 0;">
-              <i class="fas fa-sign-out-alt" style="color: red;"></i>
-            </button>
-          </form>
-        </li>
-      </ul>
+      <li class="nav-item d-flex align-items-center">
+        <span class="mr-3" style="font-size:20px;">Hi, <?php echo $user['fullname']; ?></span>
+        <a href="./profile.php" style="font-size:20px;" class="d-block mr-2"><i class="fas fa-cog"></i></a>
+        <a href="./logout.php" style="font-size:20px;"><i class="fas fa-sign-out-alt"></i></a>
+      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
@@ -218,26 +95,7 @@ $isFaculty = isset($_SESSION['user_status']) && $_SESSION['user_status'] === 'fa
 
     <!-- Sidebar -->
     <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="info">
-          <a href="./profile.php" class="d-block"><?php echo $user['fullname']; ?>
-          <i class="fas fa-cog"></i></a>
-        </div>
-      </div>
-
-      <!-- SidebarSearch Form -->
-      <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
-          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
-          <div class="input-group-append">
-            <button class="btn btn-sidebar">
-              <i class="fas fa-search fa-fw"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-
+      
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
