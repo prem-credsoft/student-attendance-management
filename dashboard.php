427192986eddfcc2a638a7415df78509e01e3501
PARTH KANJIBHAI PARMAR <parthcredsoft@gmail.com>
2024-05-11 18:52:46 +0530

    pending fees

--- dashboard.php
+++ dashboard.php
@@ -81,7 +81,7 @@
             <div class="icon">
               <i class="ion ion-cash"></i>
             </div>
-            <a href="./fees.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+            <a href="./pendingfees.php" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <div class="col-lg-3 col-6">
