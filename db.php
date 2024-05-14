8793e3745efa93fdc4f8bbf4653eb1ab536aaaa8
Prem Acharya <premcredsoft@gmail.com>
2024-05-14 19:34:53 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,16 @@
 <?php
 
-$host = "localhost";
-$dbname = "student-attendance-management";
-$user = "root";
-$password = "";
+if ($_SERVER['HTTP_HOST'] == 'localhost') {
+    $host = "localhost";
+    $dbname = "student-attendance-management";
+    $user = "root";
+    $password = "";
+} else {
+    $host = "localhost";
+    $dbname = "credsoft_rieportal";
+    $user = "credsoft_rieportal";
+    $password = "Balaji@1435";
+}
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
