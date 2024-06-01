7279b65ef1cdd879b223341463972acd8019d249
Prem Acharya <premcredsoft@gmail.com>
2024-06-01 20:33:06 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,8 +1,7 @@
 <?php
     ob_start();
-    session_start(); // Ensure the session is started before trying to modify it
+    session_start();
 
-    // Unset specific session variables
     unset($_SESSION['user_id']);
     unset($_SESSION['user_status']);
 
