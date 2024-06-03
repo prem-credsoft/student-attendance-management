ad39ce7792a5b50be21097970b884ec42cef07f8
PARTH KANJIBHAI PARMAR <parthcredsoft@gmail.com>
2024-06-03 14:26:19 +0530

    Correct Total Fees Problem

--- fees_function.php
+++ fees_function.php
@@ -16,15 +16,20 @@ if (isset($_GET['id'])) {
     $totalPaid = $totalPaidResults[0]['total_paid'] ?? 0;
 
     // Fetch total fees from the studentinfo table
-    $studentInfo = selectFromTable('studentinfo', ['total_fees'], ['id' => $studentId]);
+    $studentInfo = selectFromTable('studentinfo', ['total_fees', 'pending_fees'], ['id' => $studentId]);
     $totalFees = $studentInfo[0]['total_fees'] ?? 0;
+    $currentPendingFees = $studentInfo[0]['pending_fees'] ?? 0;
 
     // Calculate new pending fees
-    $newPendingFees = $totalFees - $totalPaid;
+    $newPendingFees = $currentPendingFees + $amount;
 
-    // Update pending fees and fee status in studentinfo table
+    // Update pending fees, total paid fees, and fee status in studentinfo table
     $feeStatus = ($newPendingFees == 0) ? 1 : 0;
-    updateTable('studentinfo', ['pending_fees' => $newPendingFees, 'fee_status' => $feeStatus], ['id' => $studentId]);
+    updateTable('studentinfo', [
+        'pending_fees' => $newPendingFees,
+        'paid_fees' => $totalPaid,
+        'fee_status' => $feeStatus
+    ], ['id' => $studentId]);
 
     header('Location: fees_receipt.php?student_id=' . $studentId . '&msg=Receipt Deleted');
 } else {
