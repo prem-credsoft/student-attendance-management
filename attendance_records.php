3b10ac83202d16f8385e60f78449269ce2455045
Prem Acharya <premcredsoft@gmail.com>
2024-06-07 12:09:04 +0530

    Fixed Responsive in Attendance records

--- attendance_records.php
+++ attendance_records.php
@@ -42,57 +42,59 @@
                                 </div>
                             </div>
                             <div class="card-body">
-                                <table id="example1" class="table table-bordered table-striped">
-                                    <thead>
-                                        <tr>
-                                            <th>Student ID</th>
-                                            <th>Student Name</th>
-                                            <th>Batch Name</th>
-                                            <th>Date</th>
-                                            <th>Status</th>
-                                            <th>Mobile Number</th>
-                                            <th>Reason</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php
-                                        $sql = "SELECT a.student_id, a.status, a.Reason, a.date, a.batch_id, s.name AS student_name, s.mobile_number, b.name AS batch_name 
+                                <div class="table-responsive">
+                                    <table id="example1" class="table table-bordered table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th>Student ID</th>
+                                                <th>Student Name</th>
+                                                <th>Batch Name</th>
+                                                <th>Date</th>
+                                                <th>Status</th>
+                                                <th>Mobile Number</th>
+                                                <th>Reason</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $sql = "SELECT a.student_id, a.status, a.Reason, a.date, a.batch_id, s.name AS student_name, s.mobile_number, b.name AS batch_name 
                                             FROM attendance a 
                                             JOIN studentinfo s ON a.student_id = s.id 
                                             JOIN batch_table b ON a.batch_id = b.id";
-                                        $attendanceRecords = executeQuery($sql);
+                                            $attendanceRecords = executeQuery($sql);
 
-                                        if ($attendanceRecords) {
-                                            foreach ($attendanceRecords as $record) {
-                                                $statusText = '';
-                                                switch ($record['status']) {
-                                                    case 0:
-                                                        $statusText = 'Present';
-                                                        break;
-                                                    case 1:
-                                                        $statusText = 'Absent';
-                                                        break;
-                                                    case 2:
-                                                        $statusText = 'Leave';
-                                                        break;
+                                            if ($attendanceRecords) {
+                                                foreach ($attendanceRecords as $record) {
+                                                    $statusText = '';
+                                                    switch ($record['status']) {
+                                                        case 0:
+                                                            $statusText = 'Present';
+                                                            break;
+                                                        case 1:
+                                                            $statusText = 'Absent';
+                                                            break;
+                                                        case 2:
+                                                            $statusText = 'Leave';
+                                                            break;
+                                                    }
+
+                                                    echo "<tr>";
+                                                    echo "<td>" . htmlspecialchars($record['student_id']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($record['student_name']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($record['batch_name']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($record['date']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($statusText) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($record['mobile_number']) . "</td>";
+                                                    echo "<td>" . htmlspecialchars($record['Reason']) . "</td>";
+                                                    echo "</tr>";
                                                 }
-
-                                                echo "<tr>";
-                                                echo "<td>" . htmlspecialchars($record['student_id']) . "</td>";
-                                                echo "<td>" . htmlspecialchars($record['student_name']) . "</td>";
-                                                echo "<td>" . htmlspecialchars($record['batch_name']) . "</td>";
-                                                echo "<td>" . htmlspecialchars($record['date']) . "</td>";
-                                                echo "<td>" . htmlspecialchars($statusText) . "</td>";
-                                                echo "<td>" . htmlspecialchars($record['mobile_number']) . "</td>";
-                                                echo "<td>" . htmlspecialchars($record['Reason']) . "</td>";
-                                                echo "</tr>";
+                                            } else {
+                                                echo "<tr><td colspan='7'>No records found.</td></tr>";
                                             }
-                                        } else {
-                                            echo "<tr><td colspan='7'>No records found.</td></tr>";
-                                        }
-                                        ?>
-                                    </tbody>
-                                </table>
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -104,7 +106,7 @@
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/buttons/1.6.1/js/dataTables.buttons.min.js"></script>
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             var table = $('#example1').DataTable({
                 "paging": true,
                 "lengthChange": true,
@@ -117,5 +119,3 @@
         });
     </script>
     <?php include ('./footer.php'); ?>
-
-
